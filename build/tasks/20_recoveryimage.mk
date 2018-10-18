b699dab97361363f6a90064eee223a98981c8677
Javier Viguera <javier.viguera@digi.com>
2018-10-18 17:41:11 +0200

    device/digi: implement recovery VFAT image
    https://jira.digi.com/browse/ADK4A-1382
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

---
+++ build/tasks/20_recoveryimage.mk
@@ -0,0 +1,28 @@
+MKIMAGE := $(HOST_OUT_EXECUTABLES)/mkimage$(HOST_EXECUTABLE_SUFFIX)
+
+INSTALLED_URAMDISK_RECOVERY_TARGET := $(PRODUCT_OUT)/uramdisk-recovery.img
+
+# 1KB blocks for mkfs.vfat
+RECOVERYIMAGE_BLOCKS := $(shell expr $(BOARD_RECOVERYIMAGE_PARTITION_SIZE) / 1024)
+
+# Recovery image included files
+RECOVERYIMAGE_FILES := $(KERNEL_IMAGE_NAME)
+RECOVERYIMAGE_FILES += $(shell echo $(TARGET_BOARD_DTS_CONFIG) | sed -e "s,[^: ]\+:,$(KERNEL_OUT)/,g")
+RECOVERYIMAGE_FILES += $(INSTALLED_BOOTSCRIPT_TARGET)
+RECOVERYIMAGE_FILES += $(INSTALLED_URAMDISK_RECOVERY_TARGET)
+
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTSCRIPT_TARGET)
+	$(call pretty,"Target DEA recovery image: $@")
+	$(call build-recoveryimage-target, $@)
+	# Remove android type recovery images
+	rm -f $(patsubst %.img,%*.img,$@)
+	$(MKIMAGE) -A arm -O linux -T ramdisk -n "Android U-Boot recovery ramdisk" -d $(recovery_ramdisk) $(INSTALLED_URAMDISK_RECOVERY_TARGET)
+	rm -f $@ && mkfs.vfat -n "RECOVERY" -S 512 -C $@ $(RECOVERYIMAGE_BLOCKS)
+	$(FAT16COPY) $@ $(RECOVERYIMAGE_FILES)
+	#
+	# Truncate VFAT recovery image:
+	#   - size of files + 10% extra space (in bytes)
+	#   - u-boot writes 512 bytes sectors so truncate at a sector boundary
+	#
+	RECOVERYIMAGE_FILES_SIZE="$$(($$(du -bc $(RECOVERYIMAGE_FILES) | tail -n1 | cut -f1) * (100 + 10) / 100))"; \
+	truncate -s $$((((RECOVERYIMAGE_FILES_SIZE + 511) / 512) * 512)) $@
