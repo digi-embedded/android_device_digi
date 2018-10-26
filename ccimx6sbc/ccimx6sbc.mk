eb35d4452e190c835609f921ecf6e1c40cabcf07
Javier Viguera <javier.viguera@digi.com>
2018-10-26 12:19:40 +0200

    ccimx6sbc: rename 'presistdata' to 'frp'
    As it's used mainly for "factory reset protection"
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- ccimx6sbc/ccimx6sbc.mk
+++ ccimx6sbc/ccimx6sbc.mk
@@ -191,4 +191,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.internel.storage_size=/sys/block/bootdev_size
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.frp.pst=/dev/block/by-name/presistdata
+	ro.frp.pst=/dev/block/by-name/frp
