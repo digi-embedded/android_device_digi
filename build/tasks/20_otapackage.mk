987914d8f01318da583e5ba572d6e9fdc164cf2f
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2018-10-30 13:10:47 +0100

    20_otapackage.mk: copy boot.img for OTA packages
    https://jira.digi.com/browse/ADK4A-1394
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

---
+++ build/tasks/20_otapackage.mk
@@ -0,0 +1,9 @@
+# Copy boot.img into OTA generation folder, so that it gets used for the OTA
+# package
+OTA_BOOT_IMAGE := $(patsubst %.zip,%,$(BUILT_TARGET_FILES_PACKAGE))/IMAGES/boot.img
+$(OTA_BOOT_IMAGE) : $(INSTALLED_BOOTIMAGE_TARGET) $(BUILT_TARGET_FILES_PACKAGE) | $(ACP)
+	@echo "Copying: $@"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) $(ACP) -fp $< $@
+
+$(INTERNAL_OTA_PACKAGE_TARGET): $(OTA_BOOT_IMAGE)
