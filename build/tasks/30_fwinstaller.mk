c94313a5adc0019f437d9784232718b78f9e114b
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2018-11-22 15:44:57 +0100

    build: generate firmware installer zip file
    This zip can be extracted to a uSD card to perform a full firmware
    installation, as documented.
    
    https://jira.digi.com/browse/ADK4A-977
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- build/tasks/30_fwinstaller.mk
+++ build/tasks/30_fwinstaller.mk
@@ -1,13 +1,41 @@
 MKIMAGE := $(HOST_OUT_EXECUTABLES)/mkimage$(HOST_EXECUTABLE_SUFFIX)
 
+FWINSTALLER_ZIP := $(PRODUCT_OUT)/install_android_fw_sd.zip
 FWINSTALLER_SRC := device/digi/$(TARGET_PRODUCT)/install_android_fw_sd.txt
 FWINSTALLER_TARGET := $(PRODUCT_OUT)/install_android_fw_sd.scr
 
+FWINSTALLER_DEPENDENCIES := \
+	$(FWINSTALLER_TARGET) \
+	$(INSTALLED_BOOTIMAGE_TARGET) \
+	$(INSTALLED_VENDORIMAGE_TARGET) \
+	$(INSTALLED_SYSTEMIMAGE) \
+	$(INSTALLED_RECOVERYIMAGE_TARGET)
+
+UBOOT_FILELIST := $(ANDROID_BUILD_TOP)/$(PRODUCT_OUT)/u-boot-*.imx
+
+FWINSTALLER_FILELIST := \
+	$(FWINSTALLER_DEPENDENCIES) \
+	$(UBOOT_FILELIST)
+
 .PHONY: fwinstaller
-fwinstaller: $(FWINSTALLER_TARGET)
+fwinstaller: $(FWINSTALLER_ZIP)
 
 $(FWINSTALLER_TARGET): $(FWINSTALLER_SRC) | $(MKIMAGE)
 	$(hide) mkdir -p $(dir $@)
 	$(hide) $(MKIMAGE) -A arm -O linux -T script -C none -n "Install script" -d $< $@
 
-droidcore: $(FWINSTALLER_TARGET)
+$(FWINSTALLER_ZIP): $(FWINSTALLER_DEPENDENCIES) $(TARGET_BOOTLOADER_IMAGE)
+	$(hide) rm -f $@
+	$(hide) ( \
+		echo "Digi Embedded for Android kit upgrader"; \
+		echo "--------------------------------------"; \
+		echo ""; \
+		echo "Version: $(BUILD_ID)"; \
+		echo "Build number: $(BUILD_NUMBER)"; \
+		echo ""; \
+		md5sum $(FWINSTALLER_FILELIST) | sed -e 's,^\([[:xdigit:]]\{32\}\).*/\([^/]\+\)$$,\1  \2,g'; \
+	) > README.txt
+	$(hide) zip -qXj $@ $(FWINSTALLER_FILELIST) README.txt
+	$(hide) rm -f README.txt
+
+droidcore: $(FWINSTALLER_ZIP)
