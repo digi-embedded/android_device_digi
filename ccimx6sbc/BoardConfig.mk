8f16703fbb10c0f7a720a768d6a894e30c59c240
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2018-11-29 11:39:29 +0100

    digi: remove support for 512MB DDR variants
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- ccimx6sbc/BoardConfig.mk
+++ ccimx6sbc/BoardConfig.mk
@@ -74,8 +74,7 @@ TARGET_BOOTLOADER_POSTFIX := imx
 TARGET_DTB_POSTFIX := -dtb
 TARGET_BOOTLOADER_CONFIG := \
 	ccimx6qsbc:ccimx6qsbc_defconfig \
-	ccimx6qsbc2GB:ccimx6qsbc2GB_defconfig \
-	ccimx6qsbc512MB:ccimx6qsbc512MB_defconfig
+	ccimx6qsbc2GB:ccimx6qsbc2GB_defconfig
 
 TARGET_KERNEL_DEFCONF := ccimx6sbc_android_defconfig
 TARGET_BOARD_DTS_CONFIG := \
