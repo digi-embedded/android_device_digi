515611394234fcf362aa40e70fd3c51339e5202f
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2018-12-27 17:43:22 +0100

    buildinfo: add more information to /etc/build
    This commits adds the following fields:
    * BUILD_ID (build version)
    * BUILD_NUMBER
    * sha1 of all repositories used.
    
    This gives way more information about the firmware currently running in the
    device.
    
    This commit also adapts the sysinfo script so that the sha1 of all the
    repositories that Digi modifies are shown in the standard output.
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- build/tasks/10_buildinfo.mk
+++ build/tasks/10_buildinfo.mk
@@ -10,6 +10,21 @@ buildinfo: $(BUILDINFO_TARGET)
 
 $(BUILDINFO_TARGET):
 	$(hide) mkdir -p $(dir $@)
-	$(hide) echo $(DATETIME) > $@
+	$(hide) ( \
+		printf "%-16s%s\n" "Version:" "$(BUILD_ID)"; \
+		printf "%-16s%s\n" "Build number:" "$(BUILD_NUMBER)"; \
+		printf "%-16s%s\n" "Date:" "$(DATETIME)"; \
+		printf "\n"; \
+		repo forall -c '\
+			printf "%-12s %-53s %s" \
+				"$${REPO_REMOTE}" \
+				"$${REPO_PROJECT} " \
+				"$$(git rev-parse --verify --short HEAD)"; \
+			if git diff-index --name-only HEAD | grep -qv "^scripts/package"; then \
+				printf '%s' -dirty; \
+			fi; \
+			printf "\n"' \
+		| sort -r -k1 \
+	) > $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(BUILDINFO_TARGET)
