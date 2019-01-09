e3b09c982dd8b6eb4ff4642ff621cfab72b7bdd8
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2019-01-09 17:05:01 +0100

    buildinfo: remove '/etc/build' and adapt sysinfo
    All the information contained in this file can now be obtained from either
    the system properties (using getprop) or from '/vendor/etc/repo-manifest.xml'
    
    The sysinfo script has been adapted to these changes. Also, it will only show
    the SHA1 of the repositories that belong to the 'sysinfo' group, instead of the
    ones that have 'github-digi' or 'mtk' as remote.
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- build/tasks/10_buildinfo.mk
+++ build/tasks/10_buildinfo.mk
@@ -1,34 +1,5 @@
 ifeq ($(strip $(TARGET_DEA_BUILDINFO)),true)
 
-# DATETIME is expected to be set from the environment (Jenkins)
-ifndef DATETIME
-DATETIME = $(shell date +'%Y%m%d%H%M%S')
-endif
-
-BUILDINFO_TARGET = $(TARGET_OUT_ETC)/build
-
-.PHONY: buildinfo
-buildinfo: $(BUILDINFO_TARGET)
-
-$(BUILDINFO_TARGET):
-	$(hide) mkdir -p $(dir $@)
-	$(hide) ( \
-		printf "%-16s%s\n" "Version:" "$(BUILD_ID)"; \
-		printf "%-16s%s\n" "Build number:" "$(BUILD_NUMBER)"; \
-		printf "%-16s%s\n" "Date:" "$(DATETIME)"; \
-		printf "\n"; \
-		repo forall -c '\
-			printf "%-12s %-53s %s" \
-				"$${REPO_REMOTE}" \
-				"$${REPO_PROJECT} " \
-				"$$(git rev-parse --verify --short HEAD)"; \
-			if git diff-index --name-only HEAD | grep -qv "^scripts/package"; then \
-				printf '%s' -dirty; \
-			fi; \
-			printf "\n"' \
-		| sort -r -k1 \
-	) > $@
-
 REPO_MANIFEST_TARGET := $(TARGET_OUT_VENDOR_ETC)/repo-manifest.xml
 
 .PHONY: repomanifest
@@ -38,6 +9,6 @@ $(REPO_MANIFEST_TARGET):
 	$(hide) mkdir -p $(dir $@)
 	$(hide) repo manifest -r --suppress-upstream-revision -o $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(BUILDINFO_TARGET) $(REPO_MANIFEST_TARGET)
+ALL_DEFAULT_INSTALLED_MODULES += $(REPO_MANIFEST_TARGET)
 
 endif
