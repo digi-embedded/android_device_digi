b999f638d4b3b3b7776418057f8c547be8c5fb7b
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2019-01-23 13:11:08 +0100

    device/digi: dea-8.0-r1.3 release
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- build_id.mk
+++ build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=dea-8.0-r1.2
+export BUILD_ID=dea-8.0-r1.3
 export BUILD_NUMBER=$(shell date +%Y%m%d)
