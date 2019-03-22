1f9e88d7ab105db0efc1c8362bed9e5a49258aec
Tatiana Leon <tatiana.leon@digi.com>
2019-03-22 15:22:00 +0100

    serial: add librxtx library
    - Add rxtx library including native and Java code as it was in 'frameworks/base'
      released dea-8.0-r1.3 ca01633b351c3678c48fb2ae0079bdbb71c7dfa3 commit.
    
    https://jira.digi.com/browse/ADK4A-1516
    
    Signed-off-by: David Escalona <david.escalona@digi.com>
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

---
+++ common/librxtxSerial/Android.mk
@@ -0,0 +1,56 @@
+#
+# Copyright 2019, Digi International Inc.
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+LOCAL_PATH:= $(call my-dir)
+
+# Build the java code
+# ============================================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := RXTXcomm
+LOCAL_REQUIRED_MODULES := librxtxSerial
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
+
+include $(BUILD_JAVA_LIBRARY)
+
+# ========================================================
+# librxtx
+# ========================================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE:= librxtxSerial
+
+
+LOCAL_SRC_FILES := \
+	lib/fuserImp.c \
+	lib/SerialImp.c
+
+LOCAL_C_INCLUDES += \
+	dalvik/libnativehelper/include/nativehelper \
+	$(JNI_H_INCLUDE) \
+	$(KERNEL_HEADERS) \
+	$(LOCAL_PATH)/lib
+
+LOCAL_CFLAGS += \
+	 -fPIC -D_GNU_SOURCE
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_SHARED_LIBRARIES := libdl liblog
+
+include $(BUILD_SHARED_LIBRARY)
+
