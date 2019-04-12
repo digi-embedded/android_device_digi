64a9e381d642853f9dd7ce1418f60a109880bbb3
Javier Viguera <javier.viguera@digi.com>
2019-04-12 11:39:49 +0200

    ccimx8xsbcpro: generate firmware installer zip package
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- build/tasks/30_fwinstaller.mk
+++ build/tasks/30_fwinstaller.mk
@@ -3,7 +3,7 @@ ifeq ($(strip $(TARGET_DEA_FWINSTALLER)),true)
 MKIMAGE := $(HOST_OUT_EXECUTABLES)/mkimage$(HOST_EXECUTABLE_SUFFIX)
 
 FWINSTALLER_ZIP := $(PRODUCT_OUT)/install_android_fw_sd.zip
-FWINSTALLER_SRC := device/digi/$(TARGET_PRODUCT)/install_android_fw_sd.txt
+FWINSTALLER_SRC := $(TARGET_DEVICE_DIR)/install_android_fw_sd.txt
 FWINSTALLER_TARGET := $(PRODUCT_OUT)/install_android_fw_sd.scr
 
 FWINSTALLER_DEPENDENCIES := \
@@ -13,18 +13,21 @@ FWINSTALLER_DEPENDENCIES := \
 	$(INSTALLED_SYSTEMIMAGE) \
 	$(INSTALLED_RECOVERYIMAGE_TARGET)
 
-UBOOT_FILELIST := $(PRODUCT_OUT)/u-boot-*.imx
+TARGET_UBOOT_IMAGES := $(foreach config,$(TARGET_BOOTLOADER_CONFIG),$(PRODUCT_OUT)/u-boot-$(firstword $(subst :, ,$(config))).imx)
+ifneq ($(strip $(TARGET_BOOTLOADER_IMXMKIMAGE_TARGETS)),)
+TARGET_UBOOT_IMAGES := $(foreach imxtgt,$(TARGET_BOOTLOADER_IMXMKIMAGE_TARGETS),$(addsuffix -$(imxtgt).imx,$(basename $(TARGET_UBOOT_IMAGES))))
+endif
 
 FWINSTALLER_FILELIST := \
 	$(FWINSTALLER_DEPENDENCIES) \
-	$(UBOOT_FILELIST)
+	$(TARGET_UBOOT_IMAGES)
 
 .PHONY: fwinstaller
 fwinstaller: $(FWINSTALLER_ZIP)
 
 $(FWINSTALLER_TARGET): $(FWINSTALLER_SRC) | $(MKIMAGE)
 	$(hide) mkdir -p $(dir $@)
-	$(hide) $(MKIMAGE) -A $(TARGET_ARCH) -O linux -T script -C none -n "Install script" -d $< $@
+	$(hide) $(MKIMAGE) -A $(TARGET_UBOOT_ARCH) -O linux -T script -C none -n "Install script" -d $< $@
 
 $(FWINSTALLER_ZIP): $(FWINSTALLER_DEPENDENCIES) $(TARGET_BOOTLOADER_IMAGE)
 	$(hide) rm -f $@
