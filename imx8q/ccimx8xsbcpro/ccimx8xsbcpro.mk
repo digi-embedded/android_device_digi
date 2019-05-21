915b6c9d8e1894ca2fb8aa32ac2d1042b1e492e3
David Escalona <david.escalona@digi.com>
2019-05-21 12:43:36 +0200

    license: add Digi and NXP EULAs in html format
    https://jira.digi.com/browse/ADK4A-1680
    
    Signed-off-by: David Escalona <david.escalona@digi.com>

--- imx8q/ccimx8xsbcpro/ccimx8xsbcpro.mk
+++ imx8q/ccimx8xsbcpro/ccimx8xsbcpro.mk
@@ -28,6 +28,10 @@ PRODUCT_COPY_FILES += \
 	device/digi/common/runtime/sysinfo:$(TARGET_COPY_OUT_VENDOR)/bin/sysinfo \
 	device/digi/common/runtime/bootanimation.zip:system/media/bootanimation.zip
 
+# License
+PRODUCT_COPY_FILES += \
+	device/digi/common/legal/license.html:$(TARGET_COPY_OUT_VENDOR)/etc/license.html
+
 # Copy device related config and binary to board
 PRODUCT_COPY_FILES += \
 	$(IMX_DEVICE_PATH)/app_whitelist.xml:system/etc/sysconfig/app_whitelist.xml \
