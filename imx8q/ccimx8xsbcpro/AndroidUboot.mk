4455887b5a85f0912a052adb410fb0e1af1c4386
Javier Viguera <javier.viguera@digi.com>
2020-05-22 13:25:19 +0200

    ccimx8x: support building u-boot for C0 silicon
    https://jira.digi.com/browse/ADK4A-1751
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- imx8q/ccimx8xsbcpro/AndroidUboot.mk
+++ imx8q/ccimx8xsbcpro/AndroidUboot.mk
@@ -5,20 +5,20 @@ ATF_CROSS_COMPILE := $(ATF_TOOLCHAIN_ABS)/aarch64-linux-androidkernel-
 
 # Make a second copy of the final artifcact removing the target from the name to avoid non-bootable U-Boots.
 define build_imx_uboot
+	UBOOT_PLATFORM="$(strip $(2))"; \
+	SCFW_PLATFORM="$$(echo $${UBOOT_PLATFORM} | cut -d'-' -f1)"; \
 	MKIMAGE_PLATFORM="iMX8QX"; \
-	SCFW_PLATFORM="8qx"; \
 	ATF_PLATFORM="imx8qx"; \
 	REV="B0"; \
-	if [ `echo $(2) | cut -d '-' -f2` == "c0" ] || [ `echo $(2) | cut -d '-' -f3` == "c0" ]; then \
-		REV=`echo C0`;  \
+	if [ "$$(echo $${UBOOT_PLATFORM} | cut -d'-' -f2)" == "c0" ]; then \
+		REV="C0";  \
 	fi; \
-	UBOOT_PLATFORM="$(strip $(2))"; \
 	$(MAKE) -C $(IMX_PATH)/arm-trusted-firmware/ PLAT=$${ATF_PLATFORM} clean; \
 	$(MAKE) -C $(IMX_PATH)/arm-trusted-firmware/ CROSS_COMPILE="$(ATF_CROSS_COMPILE)" PLAT=$${ATF_PLATFORM} bl31 -B; \
 	cp --remove-destination $(IMX_PATH)/arm-trusted-firmware/build/$${ATF_PLATFORM}/release/bl31.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/bl31.bin; \
 	cp --remove-destination $(FSL_PROPRIETARY_PATH)/imx-seco/firmware/seco/mx8qx*ahab-container.img $(IMX_MKIMAGE_PATH)/imx-mkimage/$$MKIMAGE_PLATFORM/; \
 	cp --remove-destination $(FSL_PROPRIETARY_PATH)/fsl-proprietary/mcu-sdk/imx8q/imx8qx_m4_default.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/m4_image.bin; \
-	cp --remove-destination $(DIGI_PROPRIETARY_PATH)/uboot-firmware/imx8q/$${UBOOT_PLATFORM}_scfw-tcm.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/scfw_tcm.bin; \
+	cp --remove-destination $(DIGI_PROPRIETARY_PATH)/uboot-firmware/imx8q/$${SCFW_PLATFORM}_scfw-tcm.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/scfw_tcm.bin; \
 	cp --remove-destination $(UBOOT_OUT)/u-boot.$(strip $(1)) $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/u-boot.bin; \
 	cp --remove-destination $(UBOOT_OUT)/tools/mkimage  $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/mkimage_uboot; \
 	for target in $(TARGET_BOOTLOADER_IMXMKIMAGE_TARGETS); do \
