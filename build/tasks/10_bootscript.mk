6b63ea38c0706af21c17d92ca6e65cc4401d67ad
Javier Viguera <javier.viguera@digi.com>
2020-09-13 17:35:09 +0200

    build: support building Trustfence enabled u-boot script
    https://jira.digi.com/browse/ADK4A-1773
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- build/tasks/10_bootscript.mk
+++ build/tasks/10_bootscript.mk
@@ -6,6 +6,10 @@ INSTALLED_BOOTSCRIPT_TARGET := $(PRODUCT_OUT)/boot.scr
 .PHONY: bootscript
 bootscript: $(INSTALLED_BOOTSCRIPT_TARGET)
 
-$(INSTALLED_BOOTSCRIPT_TARGET): $(BOOTSCRIPT_SRC) | $(MKIMAGE)
+$(INSTALLED_BOOTSCRIPT_TARGET): $(BOOTSCRIPT_SRC) $(wildcard $(LOCALCONF_MK)) | $(MKIMAGE) $(if $(TF_SIGN_ENABLED),$(TF_SIGN_ARTIFACT))
 	$(hide) mkdir -p $(dir $@)
 	$(hide) $(MKIMAGE) -A arm -O linux -T script -C none -n "Boot script" -d $< $@
+	$(hide) if [ "$$TF_SIGN_ENABLED" = "1" ]; then \
+		$(TF_SIGN_ARTIFACT) -p ccimx6 -b $@ $(basename $@)-signed$(suffix $@); \
+		mv $(basename $@)-signed$(suffix $@) $@; \
+	fi
