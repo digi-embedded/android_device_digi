823e4dbac0d7796aea271f6d8201006d11a4f2f2
Javier Viguera <javier.viguera@digi.com>
2020-09-13 17:35:42 +0200

    build: support building Trustfence enabled ramdisk image
    https://jira.digi.com/browse/ADK4A-1773
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- build/tasks/10_uramdisk.mk
+++ build/tasks/10_uramdisk.mk
@@ -5,6 +5,10 @@ INSTALLED_URAMDISK_TARGET := $(PRODUCT_OUT)/uramdisk.img
 .PHONY: uramdisk
 uramdisk: $(INSTALLED_URAMDISK_TARGET)
 
-$(INSTALLED_URAMDISK_TARGET): $(INSTALLED_RAMDISK_TARGET) | $(MKIMAGE)
+$(INSTALLED_URAMDISK_TARGET): $(INSTALLED_RAMDISK_TARGET) $(wildcard $(LOCALCONF_MK)) | $(MKIMAGE) $(if $(TF_SIGN_ENABLED),$(TF_SIGN_ARTIFACT))
 	$(hide) mkdir -p $(dir $@)
 	$(hide) $(MKIMAGE) -A arm -O linux -T ramdisk -n "Android U-Boot ramdisk" -d $< $@
+	$(hide) if [ "$$TF_SIGN_ENABLED" = "1" ]; then \
+		$(TF_SIGN_ARTIFACT) -p ccimx6 -i $@ $(basename $@)-signed$(suffix $@); \
+		mv $(basename $@)-signed$(suffix $@) $@; \
+	fi
