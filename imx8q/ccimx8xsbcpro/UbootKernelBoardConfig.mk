5ac5adef0ea36cd8e00fea5f1654a4bc52c96a56
Javier Viguera <javier.viguera@digi.com>
2021-01-27 14:12:01 +0100

    imx8q: ccimx8xsbcpro: add U-Boot and Linux configuration
    https://jira.digi.com/browse/ADK4A-1794
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- imx8q/ccimx8xsbcpro/UbootKernelBoardConfig.mk
+++ imx8q/ccimx8xsbcpro/UbootKernelBoardConfig.mk
@@ -1,35 +1,27 @@
 TARGET_BOOTLOADER_POSTFIX := bin
 UBOOT_POST_PROCESS := true
 
-# u-boot target for imx8qm_mek standard android
-TARGET_BOOTLOADER_CONFIG := imx8qm:imx8qm_mek_android_defconfig
-TARGET_BOOTLOADER_CONFIG += imx8qm-hdmi:imx8qm_mek_android_hdmi_defconfig
-TARGET_BOOTLOADER_CONFIG += imx8qm-md:imx8qm_mek_android_hdmi_defconfig
-# u-boot target for imx8qxp_mek standard android
-TARGET_BOOTLOADER_CONFIG += imx8qxp:imx8qxp_mek_android_defconfig
-TARGET_BOOTLOADER_CONFIG += imx8qxp-c0:imx8qxp_mek_android_defconfig
-# u-boot target for imx8dx_mek standard android
-TARGET_BOOTLOADER_CONFIG += imx8dx:imx8dx_mek_android_defconfig
-# u-boot target used by uuu for imx8dx_mek
-TARGET_BOOTLOADER_CONFIG += imx8dx-mek-uuu:imx8dx_mek_android_uuu_defconfig
+TARGET_BOOTLOADER_CONFIG := \
+        ccimx8xsbcpro2GB-C0:ccimx8x_sbc_pro2GB_android_defconfig \
+        ccimx8xsbcpro2GB-B0:ccimx8x_sbc_pro2GB_android_defconfig
 
-ifeq ($(PRODUCT_IMX_TRUSTY),true)
-  # u-boot target for imx8qm_mek standard android with trusty support
-  TARGET_BOOTLOADER_CONFIG += imx8qm-trusty:imx8qm_mek_android_trusty_defconfig
-  TARGET_BOOTLOADER_CONFIG += imx8qm-trusty-secure-unlock:imx8qm_mek_android_trusty_secure_unlock_defconfig
-  # u-boot target for imx8qxp_mek standard android with trusty support
-  TARGET_BOOTLOADER_CONFIG += imx8qxp-trusty:imx8qxp_mek_android_trusty_defconfig
-  TARGET_BOOTLOADER_CONFIG += imx8qxp-trusty-c0:imx8qxp_mek_android_trusty_defconfig
-  TARGET_BOOTLOADER_CONFIG += imx8qxp-trusty-secure-unlock:imx8qxp_mek_android_trusty_secure_unlock_defconfig
-endif
+#
+# The U-Boot imx-mkimage target define the final bootable artifact to generate:
+#
+#   - flash: final bootable artifact includes:
+#       * Standard U-Boot binary (u-boot.bin)
+#       * The firmware that runs in the SCU (scfw-tcm.bin)
+#       * The ARM Trusted Firmware that runs in the security controller (bl31.bin)
+#       * A container used by NXP to verify signed images (ahab-container.img)
+#
+#   - flash_all: like 'flash' target and:
+#       * Cortex M4 firmware (vendor/nxp/fsl-proprietary/mcu-sdk/imx8q/imx8qx_m4_default.bin)
+#
+TARGET_BOOTLOADER_IMXMKIMAGE_TARGETS := \
+        flash \
+        flash_all
 
-# u-boot target used by uuu for imx8qm_mek
-TARGET_BOOTLOADER_CONFIG += imx8qm-mek-uuu:imx8qm_mek_android_uuu_defconfig
-# u-boot target used by uuu for imx8qxp_mek
-TARGET_BOOTLOADER_CONFIG += imx8qxp-mek-uuu:imx8qxp_mek_android_uuu_defconfig
-TARGET_BOOTLOADER_CONFIG += imx8qxp-mek-c0-uuu:imx8qxp_mek_android_uuu_defconfig
-
-TARGET_KERNEL_DEFCONFIG := imx_v8_android_defconfig
+TARGET_KERNEL_DEFCONFIG := ccimx8_android_defconfig
 TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
 
 # absolute path is used, not the same as relative path used in AOSP make
