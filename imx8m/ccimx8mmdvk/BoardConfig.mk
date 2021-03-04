1fcfb76259f3fd0c4dc000b98816a81792e877b5
Javier Viguera <javier.viguera@digi.com>
2021-03-04 18:12:36 +0100

    ccimx8mmdvk: use 7 GiB disk size for partition table
    That's the available size in the ccimx8mmdvk.
    
        => mmc info
        Device: FSL_SDHC
        Manufacturer ID: 13
        OEM: 14e
        Name: Q2J55
        Bus Speed: 52000000
        Mode: MMC High Speed (52MHz)
        Rd Block Len: 512
        MMC version 5.0
        High Capacity: Yes
        Capacity: 7.1 GiB
        Bus Width: 8-bit
        Erase Group Size: 512 KiB
        HC WP Group Size: 8 MiB
        User Capacity: 7.1 GiB WRREL
        Boot Capacity: 16 MiB ENH
        RPMB Capacity: 4 MiB ENH
    
    https://jira.digi.com/browse/ADK4A-1802
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- imx8m/ccimx8mmdvk/BoardConfig.mk
+++ imx8m/ccimx8mmdvk/BoardConfig.mk
@@ -42,22 +42,7 @@ TARGET_RECOVERY_FSTAB = $(IMX_DEVICE_PATH)/fstab.digi
 
 # Support gpt
 ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
-  BOARD_BPT_INPUT_FILES += device/nxp/common/partition/device-partitions-13GB-ab_super.bpt
-  ADDITION_BPT_PARTITION = partition-table-28GB:device/nxp/common/partition/device-partitions-28GB-ab_super.bpt \
-                           partition-table-dual:device/nxp/common/partition/device-partitions-13GB-ab-dual-bootloader_super.bpt \
-                           partition-table-28GB-dual:device/nxp/common/partition/device-partitions-28GB-ab-dual-bootloader_super.bpt
-else
-  ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
-    BOARD_BPT_INPUT_FILES += device/nxp/common/partition/device-partitions-13GB-ab-no-product.bpt
-    ADDITION_BPT_PARTITION = partition-table-28GB:device/nxp/common/partition/device-partitions-28GB-ab-no-product.bpt \
-                             partition-table-dual:device/nxp/common/partition/device-partitions-13GB-ab-dual-bootloader-no-product.bpt \
-                             partition-table-28GB-dual:device/nxp/common/partition/device-partitions-28GB-ab-dual-bootloader-no-product.bpt
-  else
-    BOARD_BPT_INPUT_FILES += device/nxp/common/partition/device-partitions-13GB-ab.bpt
-    ADDITION_BPT_PARTITION = partition-table-28GB:device/nxp/common/partition/device-partitions-28GB-ab.bpt \
-                             partition-table-dual:device/nxp/common/partition/device-partitions-13GB-ab-dual-bootloader.bpt \
-                             partition-table-28GB-dual:device/nxp/common/partition/device-partitions-28GB-ab-dual-bootloader.bpt
-  endif
+BOARD_BPT_INPUT_FILES += device/digi/common/partition/device-partitions-7GB-ab_super.bpt
 endif
 
 # Vendor Interface manifest and compatibility
