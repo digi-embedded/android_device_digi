afae57b7e29d0fa4bb33a1e57514f4c2b54dddb1
Javier Viguera <javier.viguera@digi.com>
2021-05-25 14:42:58 +0200

    ccimx8xsbcpro: do not build u-boot image with CortexM4 demo
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- imx8q/ccimx8xsbcpro/AndroidUboot.mk
+++ imx8q/ccimx8xsbcpro/AndroidUboot.mk
@@ -25,14 +25,10 @@ define build_imx_uboot
 	$(MAKE) -C $(IMX_PATH)/arm-trusted-firmware/ CROSS_COMPILE="$(ATF_CROSS_COMPILE)" PLAT=$${ATF_PLATFORM} bl31 -B 1>/dev/null || exit 1; \
 	cp --remove-destination $(IMX_PATH)/arm-trusted-firmware/build/$${ATF_PLATFORM}/release/bl31.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/bl31.bin; \
 	cp --remove-destination $(FSL_PROPRIETARY_PATH)/imx-seco/firmware/seco/mx8qx*ahab-container.img $(IMX_MKIMAGE_PATH)/imx-mkimage/$$MKIMAGE_PLATFORM/; \
-	cp --remove-destination $(FSL_PROPRIETARY_PATH)/fsl-proprietary/mcu-sdk/imx8q/imx8qx_m4_default.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/m4_image.bin; \
 	cp --remove-destination $(DIGI_FIRMWARE_PATH)/uboot-firmware/imx8q/$${SCFW_PLATFORM}_scfw-tcm.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/scfw_tcm.bin; \
 	cp --remove-destination $(UBOOT_OUT)/u-boot.$(strip $(1)) $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/u-boot.bin; \
 	cp --remove-destination $(UBOOT_OUT)/tools/mkimage  $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/mkimage_uboot; \
-	for target in $(TARGET_BOOTLOADER_IMXMKIMAGE_TARGETS); do \
-		$(MAKE) -C $(IMX_MKIMAGE_PATH)/imx-mkimage/ clean; \
-		$(MAKE) -C $(IMX_MKIMAGE_PATH)/imx-mkimage/ SOC=$${MKIMAGE_PLATFORM} REV=$${REV} $${target} || exit 1; \
-		install -D $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/flash.bin $(UBOOT_COLLECTION)/u-boot-$${UBOOT_PLATFORM}-$${target}.imx; \
-		cp --remove-destination $(UBOOT_COLLECTION)/u-boot-$${UBOOT_PLATFORM}-$${target}.imx $(UBOOT_COLLECTION)/u-boot-$${UBOOT_PLATFORM}.imx; \
-	done;
+	$(MAKE) -C $(IMX_MKIMAGE_PATH)/imx-mkimage/ clean; \
+	$(MAKE) -C $(IMX_MKIMAGE_PATH)/imx-mkimage/ SOC=$${MKIMAGE_PLATFORM} REV=$${REV} flash || exit 1; \
+	cp --remove-destination $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/flash.bin $(UBOOT_COLLECTION)/u-boot-$${UBOOT_PLATFORM}.imx;
 endef
--- imx8q/ccimx8xsbcpro/UbootKernelBoardConfig.mk
+++ imx8q/ccimx8xsbcpro/UbootKernelBoardConfig.mk
@@ -5,22 +5,6 @@ TARGET_BOOTLOADER_CONFIG := \
         ccimx8xsbcpro2GB-C0:ccimx8x_sbc_pro2GB_android_defconfig \
         ccimx8xsbcpro2GB-B0:ccimx8x_sbc_pro2GB_android_defconfig
 
-#
-# The U-Boot imx-mkimage target define the final bootable artifact to generate:
-#
-#   - flash: final bootable artifact includes:
-#       * Standard U-Boot binary (u-boot.bin)
-#       * The firmware that runs in the SCU (scfw-tcm.bin)
-#       * The ARM Trusted Firmware that runs in the security controller (bl31.bin)
-#       * A container used by NXP to verify signed images (ahab-container.img)
-#
-#   - flash_all: like 'flash' target and:
-#       * Cortex M4 firmware (vendor/nxp/fsl-proprietary/mcu-sdk/imx8q/imx8qx_m4_default.bin)
-#
-TARGET_BOOTLOADER_IMXMKIMAGE_TARGETS := \
-        flash \
-        flash_all
-
 TARGET_KERNEL_DEFCONFIG := ccimx8_android_defconfig
 TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
 
