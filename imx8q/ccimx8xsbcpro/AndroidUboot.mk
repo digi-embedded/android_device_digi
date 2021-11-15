aad6b98f78a0bb8b41eae7ccb4383eb3db31368e
Javier Viguera <javier.viguera@digi.com>
2021-11-15 15:39:40 +0100

    ccimx8xsbcpro: build ZIP file with Trustfence tools
    This is needed for external (standalone) signing of the bootable artifact.
    
    https://onedigi.atlassian.net/browse/ADK4A-1752
    
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- imx8q/ccimx8xsbcpro/AndroidUboot.mk
+++ imx8q/ccimx8xsbcpro/AndroidUboot.mk
@@ -12,7 +12,27 @@ define build_m4_image
 	echo "android build without building M4 image"
 endef
 
+CSF_TEMPLATES := sign_ahab_uboot
+
+define build_trustfence_tools_zip
+	echo "== Building Trustfence tools ZIP"; \
+	TF_TOOLS_DIR="trustfence-tools-$(strip $(1))"; \
+	UBOOT_SCRIPTS_DIR="$(realpath $(UBOOT_IMX_PATH)/uboot-imx/scripts)"; \
+	( \
+		cd $(UBOOT_COLLECTION); \
+		mkdir -p $${TF_TOOLS_DIR}/csf_templates; \
+		mv mkimage*.log $${TF_TOOLS_DIR}/; \
+		install -m 0755 $${UBOOT_SCRIPTS_DIR}/sign_spl_ahab.sh $${TF_TOOLS_DIR}/trustfence-sign-uboot.sh; \
+		for f in $(CSF_TEMPLATES); do \
+			cp --remove-destination $${UBOOT_SCRIPTS_DIR}/csf_templates/$${f} $${TF_TOOLS_DIR}/csf_templates/; \
+		done; \
+		zip -qXr $${TF_TOOLS_DIR}.zip $${TF_TOOLS_DIR}; \
+		rm -rf $${TF_TOOLS_DIR}; \
+	);
+endef
+
 define build_imx_uboot
+	echo "== Building i.MX U-Boot"; \
 	UBOOT_PLATFORM="$(strip $(2))"; \
 	SCFW_PLATFORM="$$(echo $${UBOOT_PLATFORM} | cut -d'-' -f1)"; \
 	MKIMAGE_PLATFORM="iMX8QX"; \
@@ -36,6 +56,7 @@ define build_imx_uboot
 	cp --remove-destination $(UBOOT_OUT)/spl/u-boot-spl.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/u-boot-spl.bin; \
 	cp --remove-destination $(UBOOT_OUT)/tools/mkimage  $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/mkimage_uboot; \
 	$(MAKE) -C $(IMX_MKIMAGE_PATH)/imx-mkimage/ clean; \
-	$(MAKE) -C $(IMX_MKIMAGE_PATH)/imx-mkimage/ SOC=$${MKIMAGE_PLATFORM} REV=$${REV} flash_spl || exit 1; \
-	cp --remove-destination $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/flash.bin $(UBOOT_COLLECTION)/u-boot-$${UBOOT_PLATFORM}.imx;
+	$(MAKE) --no-print-directory -C $(IMX_MKIMAGE_PATH)/imx-mkimage/ SOC=$${MKIMAGE_PLATFORM} REV=$${REV} flash_spl 2>&1 | tee $(UBOOT_COLLECTION)/mkimage.log || exit 1; \
+	cp --remove-destination $(IMX_MKIMAGE_PATH)/imx-mkimage/$${MKIMAGE_PLATFORM}/flash.bin $(UBOOT_COLLECTION)/u-boot-$${UBOOT_PLATFORM}.imx; \
+	$(call build_trustfence_tools_zip, $(2))
 endef
