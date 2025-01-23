b399b04b82c93f1f56f7f0faa19f2823c5a32e4e
Javier Viguera <javier.viguera@digi.com>
2025-01-23 18:41:47 +0100

    ccimx8xsbcpro: decrease cma to allow booting on 2GB devices
    Signed-off-by: Javier Viguera <javier.viguera@digi.com>

--- imx8q/ccimx8xsbcpro/BoardConfig.mk
+++ imx8q/ccimx8xsbcpro/BoardConfig.mk
@@ -88,7 +88,7 @@ BOARD_BOOTCONFIG += androidboot.hardware=digi
 BOARD_BOOTCONFIG += androidboot.fbTileSupport=enable
 
 # memory config
-BOARD_KERNEL_CMDLINE += cma=928M@0x960M-0xfc0M transparent_hugepage=never
+BOARD_KERNEL_CMDLINE += cma=800M transparent_hugepage=never
 
 # display config
 BOARD_BOOTCONFIG += androidboot.lcd_density=240
